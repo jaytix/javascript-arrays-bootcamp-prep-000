2cc4d3333bf20c2a92e1bdcd9c28c58e0e08103f
Joshua Ticzon <ticzon.joshua@gmail.com>
2017-10-22 00:11:30 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -31,3 +31,8 @@ function destructivelyAddElementToEndOfArray(array, element) {
 function accessElementInArray(array, index) {
   return array[index];
 }
+
+function destructivelyRemoveElementFromBeginningOfArray (array) {
+  array.shift()
+  return array;
+}
