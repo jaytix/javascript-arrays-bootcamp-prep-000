e90cc9057a158c643b312a446a3407bdc46a5491
Joshua Ticzon <ticzon.joshua@gmail.com>
2017-10-22 00:04:44 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,7 +3,9 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 function addElementToBeginningOfArray(array, element){
   var arr = []
   arr.push(element);
-  arr.push(array);
+  for (var i = 0; i < array.length; i++) {
+    arr.push(array[i]);
+  }
   return arr;
 }
 
