df3955bfd532d53b6c7dcbb6571936e5aa65da58
Joshua Ticzon <ticzon.joshua@gmail.com>
2017-10-22 00:08:04 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -13,3 +13,12 @@ function destructivelyAddElementToBeginningOfArray(array, element){
   array.unshift(element);
   return array;
 }
+
+function addElementToEndOfArray(array, element) {
+  var arr = [];
+  for (var i = 0; i < array.length; i++){
+    arr.push(array[i]);
+  }
+  arr.push(element);
+  return arr;
+}
