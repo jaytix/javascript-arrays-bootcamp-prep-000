322ea21fffe9913965d048470b68e4ff989a7fad
Joshua Ticzon <ticzon.joshua@gmail.com>
2017-10-22 00:17:08 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -51,3 +51,13 @@ function destructivelyRemoveElementFromEndOfArray(array) {
   array.pop();
   return array;
 }
+
+function removeElementFromEndOfArray(array) {
+  var arr = [];
+
+  for (var i = 0; i < array.length - 1; i++){
+    arr.push(array[i]);
+  }
+
+  return arr;
+}
